e0a21a76f53839fc12d7ca3c5a975ea8d7814e62
sikandermerchant <sikander.merchant@gmail.com>
2021-03-08 18:33:10 +0000

    Added JS code for filling the large glass

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 - CSS based large and small cups are created
 - on clicking a cup the class 'full' is added to the cup.
 - when clicked on an empty cup, all previous empty cups are also filled
+- the large cup is calculated based on the full cups using
+- percentage is calculated using = `${fullCups / totalCups * 330}px`;
+- litres remaining is calculated = `${2 - (250 * fullCups / 1000)}L`
--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const litres = document.getElementById('litres');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
 
+updateBigCup();
 smallCups.forEach((cup, index) => {
   cup.addEventListener('click', () => highlightCups(index))
 });
@@ -18,4 +19,25 @@ const highlightCups = (index) => {
       cup.classList.remove('full');
     }
   });
+  updateBigCup();
+}
+function updateBigCup()
+{
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+  if(fullCups === 0){
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  }else{
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${fullCups / totalCups * 330}px`;
+    percentage.innerText = `${fullCups / totalCups * 100}%`
+  }
+  if(fullCups === totalCups){
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0
+  }else{
+    remained.style.visibility = 'visible';
+    litres.innerText = `${2 - (250 * fullCups / 1000)}L`
+  }
 }
